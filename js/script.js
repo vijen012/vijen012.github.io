29c77afed5bddf83e169325ef61f8eeff224ae6a
Bhagwati Prasad. Malav <bmalav@oradev.oraclecorp.com>
2016-12-01 07:30:08 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+    var d = new Date('2017-01-17T00:00:00');
+    
+	$('#counter').countdown({     
+		timestamp : (new Date()).getTime() + Math.abs(d - new Date())  //17 date millisecond - current date milliseconds
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+	/* -- Using Modernizr to check if the "required" and "placeholder" attributes are supported -- */
+
+	if (!Modernizr.input.placeholder) {
+		$('.email').val('Input your e-mail address here...');
+		$('.email').focus(function() {
+			if($(this).val() == 'Input your e-mail address here...') {
+				$(this).val('');
+			}
+		});
+	}
+
+	// for detecting if the browser is Safari
+	var browser = navigator.userAgent.toLowerCase();
+
+	if(!Modernizr.input.required || (browser.indexOf("safari") != -1 && browser.indexOf("chrome") == -1)) {
+		$('form').submit(function() {
+			$('.popup').remove();
+			if(!$('.email').val() || $('.email').val() == 'Input your e-mail address here...') {
+				$('form').append('<p class="popup">Please fill out this field.</p>');
+				$('.email').focus();
+				return false;
+			}
+		});
+		$('.email').keydown(function() {
+			$('.popup').remove();
+		});
+		$('.email').blur(function() {
+			$('.popup').remove();
+		});
+	}
+
+
+});
